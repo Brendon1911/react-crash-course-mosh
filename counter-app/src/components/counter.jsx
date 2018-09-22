2f065d2a4602862c3129025bf56f08b98d4fcb11
Brendon Richards <brendonrichards93@gmail.com>
2018-09-22 03:58:47 +0000

    Create dynamic styling

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
@@ -6,12 +6,19 @@ class Counter extends Component {
   };
   
   render () {
+    
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+  
+  getBadgeClasses () {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
   
   formatCount () {
