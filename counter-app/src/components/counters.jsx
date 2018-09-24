f6fae8365a37f8e13cd59ce7420fac4dc33b4ccd
Brendon Richards <brendonrichards93@gmail.com>
2018-09-24 08:39:38 +0000

    Remove the local state

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,18 +11,26 @@
     ]
    };
    
+   handleIncrement = counter => {
+     const counters = [...this.state.counters];
+     const index = counters.indexOf(counter);
+     counters[index] = { ...counter };
+     counters[index].value++;
+     this.setState({ counters });
+   };
+   
    handleReset = () => {
      const counters = this.state.counters.map(c => {
        c.value = 0;
        return c;
      });
      this.setState({ counters });
-   }
+   };
    
    handleDelete = counterId => {
      const counters = this.state.counters.filter(c => c.id !== counterId);
      this.setState({ counters });
-   }
+   };
    
    render () {
       return (
@@ -36,6 +44,7 @@
           <Counter 
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
           )}
