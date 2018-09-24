f2a100ad82a4d993c7ec1a734d1e4cdaa6bb2282
Brendon Richards <brendonrichards93@gmail.com>
2018-09-24 07:00:04 +0000

    Finish delete handling functionality

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,17 +11,21 @@
     ]
    };
    
-   handleDelete = () => {
-     console.log('Event handler called');
+   handleDelete = counterId => {
+     const counters = this.state.counters.filter(c => c.id !== counterId);
+     this.setState({ counters });
    }
    
    render () {
       return (
         <div>
-          { this.state.counters.map(counter => 
-          <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.is}>
-          </Counter>
-          ) }
+          {this.state.counters.map(counter => 
+          <Counter 
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+          )}
         </div>
       );
    }
